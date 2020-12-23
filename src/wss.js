24e3be1c050b45a877a1a0085cd8f114ee52715b
Bill <bill.chandos@gmail.com>
2020-12-23 21:20:53 +0000

    handle receiving messages

--- src/wss.js
+++ src/wss.js
@@ -1,13 +1,15 @@
 const { Server } = require('ws');
 const { authenticateWebsocket } = require('./jwt');
+const sequelize = require('./models');
 
+const Message = sequelize.models.Message;
 
 const wss = new Server({ noServer: true });
 
 wss.on('connection', (ws) => {
     console.log('Client connected');
-    ws.send(JSON.stringify({'msg': 'You have connected to the websocket'}));
-    ws.on('message', (msg) => {
+    // ws.send(JSON.stringify({'msg': 'You have connected to the websocket'}));
+    ws.on('message', async (msg) => {
         const msgJson = JSON.parse(msg);
         let authenticated;
         if (msgJson.token) {
@@ -19,7 +21,25 @@ wss.on('connection', (ws) => {
             ws.close(1000, 'Not authorized');
         }
         else {
-            ws.send(JSON.stringify({'msg': 'You have been authorized!'}));
+            let response;
+            if (msgJson.type === 'new-message') {
+                const message = await Message.create({
+                    text: msgJson.contents.text,
+                    sentAt: msgJson.contents.sentAt,
+                    size: msgJson.contents.size,
+                });
+                message.setUser(msgJson.contents.UserId);
+                message.setConversation(msgJson.contents.ConversationId);
+                await message.save();
+                response = await Message.findByPk(message.id, { include: 'User'} );
+            }
+
+            for (let client of wss.clients) {
+                if (client.readyState === 1) {
+                    client.send(JSON.stringify(response));
+                }
+            }
+            // ws.send(JSON.stringify(response));
         }
     });
     ws.on('close', () => {
