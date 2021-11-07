53986c9e172b31c662e3511be3b39f6fa7aee210
bchandos <contact@billchandos.dev>
2021-11-07 22:49:37 +0000

    add JSON parsing to middleware

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ const routes = require('./routes');
 const app = express();
 
 app.use(cors());
-// app.use(express.json());
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
